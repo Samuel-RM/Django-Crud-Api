39a1ca4843bbdc0c49f6bf46df64c1b1c1d488b8
¨Samuel <samiriverame07@gmail.com>
2023-04-27 01:19:48 -0600

    Get Task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import {TasksPage} from './pages/TasksPage'
 import {TaskFormPage} from "./pages/TaskFormPage";
-
+import { Navigations } from "./components/Navigations";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+    <Navigations />
+    
+    <Routes>
         <Route path='/' element={<Navigate to="/tasks" />} />
         <Route path='/tasks' element={<TasksPage />} />
         <Route path='/tasks-create' element={<TaskFormPage />} />
-      </Routes>
+    </Routes>
     </BrowserRouter>
   )
 }
--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 export function Navigations() {
   return (
     <div>
-        <h1>task App</h1>
-        <Link to ="/tasks-create">creste task</Link>
+        <Link to='/tasks'>
+          <h1>Task App</h1>
+        </Link> 
+        <Link to ="/tasks-create">create task</Link>
     </div>
   )
 }
