fbc5489bb627aac340a8703dfb7716eeba610139
¨Samuel <samiriverame07@gmail.com>
2023-04-26 20:58:12 -0600

    Starting react

---
+++ client/src/components/Navigations.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export function Navigations() {
+  return (
+    <div>
+        <h1>task App</h1>
+        <Link to ="/tasks-create">creste task</Link>
+    </div>
+  )
+}
+    
